91b4ec317a787982d66fff45dd839e3804c584be
dimaparhatsky <166734211+dimaparhatsky@users.noreply.github.com>
2024-07-22 10:36:33 +0500

    Add files via upload

---
+++ burger.js
@@ -0,0 +1,24 @@
+const BURGER_OPENED_CLASSNAME = 'burger_open';
+const BURGER_BTN_OPENED_CLASSNAME = 'burger-btn_open';
+const BODY_FIXED_CLASSNAME_BURGER = 'body_fixed';
+
+const bodyNodeBurger = document.querySelector('body');
+const burgerNode = document.querySelector('.js-burger');
+const burgerBtnNode = document.querySelector('.js-burger-btn');
+const burgerContentNode = document.querySelector('.js-burger__content')
+
+burgerBtnNode.addEventListener('click', toggleBurger);
+
+burgerNode.addEventListener('click', (event) => {
+    const isClickOutsideContent = !event.composedPath().includes(burgerContentNode)
+
+    if (isClickOutsideContent) {
+        toggleBurger();
+    }
+})
+
+function toggleBurger() {
+    burgerNode.classList.toggle(BURGER_OPENED_CLASSNAME);
+    bodyNodeBurger.classList.toggle(BODY_FIXED_CLASSNAME_BURGER);
+    burgerBtnNode.classList.toggle(BURGER_BTN_OPENED_CLASSNAME);
+}
